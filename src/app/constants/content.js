f062923e77a1c5bd5631dcc1b5de0b32fa40529d
kyakivanfrank <kyakivanfrank@gmail.com>
2024-01-11 17:19:04 +0300

    initial commit

---
+++ src/app/constants/content.js
@@ -0,0 +1,21 @@
+export const hero_section = {
+  title: [
+    "Frank Kyakusse",
+    1000,
+    "Web Developer",
+    1000,
+    "Mobile Developer",
+    1000,
+    "Tech Enthusiast",
+    1000,
+  ],
+  description:
+    "Imagine something that is simple but does everything,  I believe that Minimalism is the new complexity...",
+};
+
+export const about_section = {
+  about_statement:
+    "Hi there, I'm Frank Ivan Kyakusse. A Front-end web developer and the creator of studioFrank I'm thrilled by sleek but wholly inclusive designs. In this era, minimalism is the new complexity. What matters most is that a platform meets all the desired use-cases without excessive UI elements. A single button can unravel different features in various scenarios, leading to a clean and scalable UI. That's what I aim for in my work. Please feel free to navigate through My portfolio, You never know, something might catch your attention.",
+ };
+
+ 
